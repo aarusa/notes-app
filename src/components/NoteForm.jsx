1c10cb3cbd717ff2d3a5a84b5973c2eca2f706c7
Arusha Shahi <29802414+aarusa@users.noreply.github.com>
2026-07-23 17:39:45 +1000

    Initial commit

---
+++ src/components/NoteForm.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+const NoteForm = () => {
+
+    const [title, setTitle] = useState('');
+    const [priority, setPriority] = useState('Medium');
+    const [category, setCategory] = useState('Personal');
+    const [description, setDescription] = useState('');
+
+    return ( 
+        <form className="mb-6">
+            <div className="mb-4">
+                <label htmlFor="title" className="block font-semibold">
+                    Title
+                </label>
+                <input 
+                    type="text" 
+                    className="w-full p-2 border rounded-lg"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)} 
+                />
+            </div>
+            <div className="mb-4">
+                <label htmlFor="priority" className="block font-semibold">
+                    Priority
+                </label>
+                <select
+                    type="text" 
+                    className="w-full p-2 border rounded-lg"
+                    value={priority}
+                    onChange={(e) => setPriority(e.target.value)} 
+                >
+                    <option value='High'>🔴 High</option>
+                    <option value='Medium'>🟠 Medium</option>
+                    <option value='Low'>🟢 Low</option>
+                </select>
+            </div>
+            <div className="mb-4">
+                <label htmlFor="category" className="block font-semibold">
+                    Category
+                </label>
+                <select
+                    type="text" 
+                    className="w-full p-2 border rounded-lg"
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)} 
+                >
+                    <option value='Work'>💼 Work</option>
+                    <option value='Personal'>🏠 Personal</option>
+                    <option value='Idea'>💡 Idea</option>
+                </select>
+            </div>
+            <div className="mb-4">
+                <label htmlFor="description" className="block font-semibold">
+                    Description
+                </label>
+                <textarea 
+                    type="text" 
+                    className="w-full p-2 border rounded-lg"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)} 
+                ></textarea>
+            </div>
+            <button className="w-full bg-purple-600 text-white py-2 rounded-lg cursor-pointer hover:bg-purple-700">Add Note</button>
+        </form>
+     );
+}
+ 
+export default NoteForm;
