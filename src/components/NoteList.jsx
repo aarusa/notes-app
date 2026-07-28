8e9e7b251493e2e117631350f875943c965fe41c
Arusha Shahi <29802414+aarusa@users.noreply.github.com>
2026-07-28 17:54:26 +1000

    Note component is created

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,3 +1,5 @@
+import Note from "./Note";
+
 const NoteList = ({notes, deleteNote}) => {
     if (notes.length === 0) {
         return(
@@ -10,24 +12,7 @@ const NoteList = ({notes, deleteNote}) => {
     return ( 
         <div className="space-y-4">
             { notes.map((note) => (
-                <div key={note.id} className="p-4 bg-white rounded-lg shadow-m border-l-4">
-                    <h3 className="text-lg font-bold">
-                        {note.title}
-                    </h3>
-                    <p className="text-sm text-gray-600">
-                        <strong>Category: </strong>{note.category}
-                    </p>
-                    <p className="text-sm text-gray-600">
-                        <strong>Priority: </strong>{note.priority}
-                    </p>
-                    <p className="mt-2">
-                        {note.description}
-                    </p>
-
-                    <button onClick={() => deleteNote(note.id)} className='mt-3 text-red-500 cursor-pointer transition hover:text-red-700'>
-                        🗑️ Delete
-                    </button>
-                </div>
+                <Note key={note.id} note={note} deleteNote={deleteNote} />
             )) }
         </div>
      );
